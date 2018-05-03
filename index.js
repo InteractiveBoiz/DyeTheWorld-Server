ef6b4c15cdaa086625efb6309fe634b1f5f983b3
Martin <martjohannsen@gmail.com>
2018-05-03 12:35:31 +0200

    Added feature: Post request for point, Tools added: Mongoose(and necessary dependencies)

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const path = __dirname + "/public/";
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+const Point = require('./models/point');
 
 const MongoClient = require('mongodb').MongoClient;
 var mongoURL = "mongodb://MongoDB-Dev1:1234@dyetheworld-dev-shard-00-00-si1d6.mongodb.net:27017,dyetheworld-dev-shard-00-01-si1d6.mongodb.net:27017,dyetheworld-dev-shard-00-02-si1d6.mongodb.net:27017/DyeTheWorldDB?ssl=true&replicaSet=DyeTheWorld-Dev-shard-0&authSource=admin";
-
+mongoose.connect(mongoURL, {
+	useMongoClient: true
+	}
+);
 // For at de enkelte filer kan anvendes fra browser:
 app.use(bodyParser.urlencoded({extended : true}));
 
@@ -16,12 +22,12 @@ app.use(bodyParser.json());
 
 app.get('/polygons', function(req, res)
 {
-	MongoClient.connect(mongoURL, function(err, db)
-	{
+  MongoClient.connect(mongoURL, function(err, db)
+  {
 	  var col = db.collection('Polygon');
 
-		col.find().toArray(function (err, result)
-		{
+    col.find().toArray(function (err, result)
+    {
 			console.log(result);
 			res.json(result);
 		});
@@ -43,5 +49,54 @@ app.get('/points', function(req, res)
 		db.close();
 	});
 });
+
+// CREATE
+app.post('/createpoint', (req, res, next) => {  
+	const point = new Point({
+		_id: new mongoose.Types.ObjectId(),
+		ID: req.body.ID,
+		Latitude: req.body.Latitude,
+		Longitude: req.body.Longitude,
+		//Date: req.body.Date
+	});
+	point.save().then(result => {
+		console.log(result);
+		res.status(201).json({
+			message: "Handling POST requests to /createpoint",
+			createdPoint: result
+		});
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
+	});
+});
+		
+	app.post('/createpoint', (req, res, next) => {  
+	
+		const point = new Point({
+			_id: new mongoose.Types.ObjectId(),
+			ID: req.body.ID,
+			Latitude: req.body.Latitude,
+			Longitude: req.body.Longitude,
+			//Date: req.body.Date
+		});
+		point
+			.save()
+			.then(result => {
+				console.log(result);
+				res.status(201).json({
+					message: "Handling POST requests to /createpoint",
+					createdPoint: result
+				});
+			})
+			.catch(err => {
+				console.log(err);
+				res.status(500).json({
+					error: err
+				});
+			});
+		});
 
 app.listen(process.env.PORT || 3000);
---
+++ models/point.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const pointSchema = mongoose.Schema({
+  ObjectId: mongoose.Schema.Types.ObjectId,
+  ID: mongoose.Schema.Types.Number,
+  Latitude: mongoose.Schema.Types.Number,
+  Longitude: mongoose.Schema.Types.Number,
+  //TimeStamp: mongoose.Schema.Types.Date
+});
+
+module.exports = mongoose.model('Point', pointSchema, 'Point');
