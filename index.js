b0cf8fba150e74b587f136da5cc5c586ea5e2822
Martin <martjohannsen@gmail.com>
2018-05-06 16:46:01 +0200

    Refactoring: Added several files for each routepath. Makes it more obvious where each part of the source code is contained. Look to pointRoute.js in the routes folder, for routes that handles points etc...! Added Cors dependency for handling several route files! MJ

--- index.js
+++ index.js
@@ -1,104 +1,14 @@
 const express = require('express');
 const app = express();
-const path = __dirname + "/public/";
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
-const Point = require('./models/point');
-const Polygon = require('./models/polygon');
+// Makes it possible to access several files containing routes without it crashing.
+var cors = require('cors');
+app.use(cors()); 	
 
-const MongoClient = require('mongodb').MongoClient;
-var mongoURL = "mongodb://MongoDB-Dev1:1234@dyetheworld-dev-shard-00-00-si1d6.mongodb.net:27017,dyetheworld-dev-shard-00-01-si1d6.mongodb.net:27017,dyetheworld-dev-shard-00-02-si1d6.mongodb.net:27017/DyeTheWorldDB?ssl=true&replicaSet=DyeTheWorld-Dev-shard-0&authSource=admin";
-mongoose.connect(mongoURL, {
-	useMongoClient: true	
-});
-// For at de enkelte filer kan anvendes fra browser:
-app.use(bodyParser.urlencoded({extended : true}));
+const pointModule = require('./routes/pointRoute');
+app.use(pointModule);
 
-// For at css skal kunne anvendes
-
-app.use(express.static(path));
-app.use(bodyParser.json());
-
-app.get('/polygons', function(req, res)
-{
-  MongoClient.connect(mongoURL, function(err, db)
-  {
-	  var col = db.collection('Polygon');
-
-    col.find().toArray(function (err, result)
-    {
-			console.log(result);
-			res.json(result);
-		});
-		db.close();
-	});
-});
-
-app.get('/points', function(req, res)
-{
-	MongoClient.connect(mongoURL, function(err, db)
-	{
-	  var col = db.collection('Point');
-
-		col.find().toArray(function (err, result)
-		{
-			console.log(result);
-			res.json(result);
-		});
-		db.close();
-	});
-});
-
-// CREATE
-app.post('/createpoint', (req, res, next) => {  
-	const point = new Point({
-		_id: new mongoose.Types.ObjectId(),
-		ID: req.body.ID,
-		PolygonID: req.body.PolygonID,
-		Latitude: req.body.Latitude,
-		Longitude: req.body.Longitude
-		//Date: req.body.Date
-	});
-	point.save().then(result => {
-		console.log(result);
-		res.status(201).json({
-			message: "Handling POST requests to /createpoint",
-			createdPoint: result
-		});
-	}).catch(err => {
-		console.log(err);
-		res.status(500).json({
-			error: err
-		});
-	});
-});
-		
-//Create polygon
-app.post('/createpolygon', (req, res, next) => {  
-
-	const polygon = new Polygon({
-		_id: new mongoose.Types.ObjectId(),
-		ID: req.body.ID,
-		TeamID: req.body.TeamID,
-		AreaImperial: req.body.AreaImperial
-		//Date: req.body.Date
-	});
-	polygon
-		.save()
-		.then(result => {
-			console.log(result);
-			res.status(201).json({
-				message: "Handling POST requests to /createpolygon",
-				createdPolygon: result
-			});
-		})
-		.catch(err => {
-			console.log(err);
-			res.status(500).json({
-				error: err
-			});
-		});
-	});
+const polygonModule = require('./routes/polygonRoutes');
+app.use(polygonModule);
 
 app.listen(process.env.PORT || 3000);
---
+++ routes/pointRoute.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+const Point = require('../models/point');
+const Polygon = require('../models/polygon');
+
+const MongoClient = require('mongodb').MongoClient;
+var mongoURL = "mongodb://MongoDB-Dev1:1234@dyetheworld-dev-shard-00-00-si1d6.mongodb.net:27017,dyetheworld-dev-shard-00-01-si1d6.mongodb.net:27017,dyetheworld-dev-shard-00-02-si1d6.mongodb.net:27017/DyeTheWorldDB?ssl=true&replicaSet=DyeTheWorld-Dev-shard-0&authSource=admin";
+mongoose.connect(mongoURL, {
+	useMongoClient: true	
+});
+
+
+// For at css skal kunne anvendes
+
+app.use(bodyParser.json());
+
+app.get('/points', function(req, res)
+{
+	MongoClient.connect(mongoURL, function(err, db)
+	{
+	  var col = db.collection('Point');
+
+		col.find().toArray(function (err, result)
+		{
+			console.log(result);
+			res.json(result);
+		});
+		db.close();
+	});
+});
+
+// CREATE
+app.post('/createpoint', (req, res, next) => {  
+	const point = new Point({
+		_id: new mongoose.Types.ObjectId(),
+		ID: req.body.ID,
+		PolygonID: req.body.PolygonID,
+		Latitude: req.body.Latitude,
+		Longitude: req.body.Longitude
+		//Date: req.body.Date
+	});
+	point.save().then(result => {
+		console.log(result);
+		res.status(201).json({
+			message: "Handling POST requests to /createpoint",
+			createdPoint: result
+		});
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error: err
+		});
+	});
+});
+
+module.exports = app;
